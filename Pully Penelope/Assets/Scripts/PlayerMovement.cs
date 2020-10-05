28de9c5c23034a1ca3c9ebe45eaf68a0580dfc26
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2020-10-04 20:49:48 -0500

    Added player movement

---
+++ Pully Penelope/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    private new Rigidbody2D rigidbody;
+    private Vector3 positionChange;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        positionChange = Vector3.zero;
+        positionChange.x = Input.GetAxisRaw("Horizontal");
+        positionChange.y = Input.GetAxisRaw("Vertical");
+
+        /*if (positionChange != Vector3.zero)
+        {
+            MovePlayer();
+        }*/
+    }
+
+    private void FixedUpdate()
+    {
+        if (positionChange != Vector3.zero)
+        {
+            MovePlayer();
+        }
+    }
+
+    private void MovePlayer()
+    {
+        rigidbody.MovePosition(transform.position + positionChange.normalized * moveSpeed * Time.deltaTime);
+    }
+}
