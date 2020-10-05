0cc20ec275aebdfc48aeb0bf839a60a7afcba30e
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2020-10-05 18:42:07 -0500

    Added animations

--- Pully Penelope/Assets/Scripts/PlayerMovement.cs
+++ Pully Penelope/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed;
 
+    private Animator animator;
     private new Rigidbody2D rigidbody;
     private Vector3 positionChange;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -29,9 +31,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        MoveAndAnimate();
+    }
+
+    private void MoveAndAnimate()
+    {
         if (positionChange != Vector3.zero)
         {
             MovePlayer();
+            animator.SetFloat("moveX", positionChange.x);
+            animator.SetFloat("moveY", positionChange.y);
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
     }
 
